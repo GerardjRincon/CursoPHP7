9559c88c8a2abbc54243316be71fc88d6bc33ef7
Gerard <grf1996@gmail.com>
2018-02-13 05:44:31 -0300

    2._ Curso PHP 7 - Variables,Constantes y comentarios

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$var1 = 2;
+$cvar2 = 2.4;
+$bol = false;
+$s = "Esto es un string";
+$S= " estoe s una S mayuscula";
+
+$a= "<br/>";
+
+
+// esto es un comentario 1
+
+/* 
+
+    esto es un comentario multiple
+
+*/
+
+# esto es otro comentario
+
+define ('UNO','Esto es un 1');
+
+
+
+
+
+
+
+
+
+
+?>
